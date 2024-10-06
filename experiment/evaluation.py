8dbe11b78330b2de6cbce0f58aa21e4d02f8374e
IT-Gentleman <gentleman@kakao.com>
2024-10-06 21:38:43 +0900

    UPDATE evaluation.py(prev: execution.py) for Jetson Orin Nano's compatibility and etc.

--- experiment/evaluation.py
+++ experiment/evaluation.py
@@ -183,6 +183,62 @@ class earlyTTFS(nn.Module):
                 break
             
         return torch.stack(spk_out)
+
+class zeroTTFS(nn.Module):
+    def __init__(self, input_channels=1, output_channels=10, beta=0.95, input_size=28):
+        super(zeroTTFS, self).__init__()
+        self.conv1 = nn.Conv2d(input_channels, 32, kernel_size=3, stride=1, padding=1)
+        self.lif1 = snn.Leaky(beta=beta, init_hidden=True)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.lif2 = snn.Leaky(beta=beta, init_hidden=True)
+
+        # Compute the size after convolution and pooling layers
+        size_after_conv1 = input_size  # Since padding=1, stride=1, kernel_size=3
+        size_after_pool1 = size_after_conv1 // 2  # After first pooling
+        size_after_conv2 = size_after_pool1  # Conv layer maintains size
+        size_after_pool2 = size_after_conv2 // 2  # After second pooling
+        final_size = size_after_pool2
+
+        # Define fully connected layers with dynamic input size
+        self.fc1 = nn.Linear(64 * final_size * final_size, 100)
+        self.lif3 = snn.Leaky(beta=beta, init_hidden=True)
+        self.fc2 = nn.Linear(100, output_channels)
+        self.lif4 = snn.Leaky(beta=beta, init_hidden=True)
+
+    def forward(self, x):
+        self.lif1.init_leaky()
+        self.lif2.init_leaky()
+        self.lif3.init_leaky()
+        self.lif4.init_leaky()
+
+        num_steps = x.shape[0]
+        spk_out = []
+        batch_size = x.size(1)
+        not_spiked = torch.ones(batch_size, dtype=torch.bool, device=x.device)
+        for step in range(num_steps):
+            #x_t = x[step].clone()
+            x_t = x[step]
+            x_t[~not_spiked] = 0.0
+            #x_t = x_t * not_spiked.view(-1, 1, 1, 1).float()
+            cur1 = self.conv1(x_t)
+            spk1= self.lif1(cur1)
+            spk1 = self.pool(spk1)
+            cur2 = self.conv2(spk1)
+            spk2 = self.lif2(cur2)
+            spk2 = self.pool(spk2)
+            spk2_flat = spk2.view(spk2.size(0), -1)
+            cur3 = self.fc1(spk2_flat)
+            spk3 = self.lif3(cur3)
+            cur4 = self.fc2(spk3)
+            spk4 = self.lif4(cur4)
+            spk_out.append(spk4)
+            #spiked_now = spk4.sum(dim=1) > 0
+            #newly_spiked = spiked_now & not_spiked
+            newly_spiked = (spk4.sum(dim=1)>0) & not_spiked
+            not_spiked[newly_spiked] = False
+            
+        return torch.stack(spk_out)
     
 class earlyzeroTTFS(nn.Module):
     def __init__(self, input_channels=1, output_channels=10, beta=0.95, input_size=28):
@@ -219,8 +275,8 @@ class earlyzeroTTFS(nn.Module):
         for step in range(num_steps):
             #x_t = x[step].clone()
             x_t = x[step]
-            #x_t[~not_spiked] = 0.0
-            x_t = x_t * not_spiked.view(-1, 1, 1, 1).float()
+            x_t[~not_spiked] = 0.0
+            #x_t = x_t * not_spiked.view(-1, 1, 1, 1).float()
             cur1 = self.conv1(x_t)
             spk1= self.lif1(cur1)
             spk1 = self.pool(spk1)
@@ -237,10 +293,10 @@ class earlyzeroTTFS(nn.Module):
             #newly_spiked = spiked_now & not_spiked
             newly_spiked = (spk4.sum(dim=1)>0) & not_spiked
             not_spiked[newly_spiked] = False
-            #if not not_spiked.any():
-            #    for _ in range(step + 1, num_steps):
-            #        spk_out.append(torch.zeros_like(spk4))
-            #    break
+            if not not_spiked.any():
+                for _ in range(step + 1, num_steps):
+                    spk_out.append(torch.zeros_like(spk4))
+                break
             
         return torch.stack(spk_out)
 '''
@@ -521,6 +577,8 @@ def snn_train_pipeline(args, paths, device, train_loader, test_loader, channels)
     # Hyperparameter
     if args.latency_early_stopping and args.latency_propagate_zero:
         model = earlyzeroTTFS(channels.input, channels.output, args.beta, channels.size)
+    elif args.latency_propagate_zero:
+        model = zeroTTFS(channels.input, channels.output, args.beta, channels.size)
     elif args.latency_early_stopping:
         model = earlyTTFS(channels.input, channels.output, args.beta, channels.size)
     else:
@@ -626,12 +684,14 @@ def main(args, paths):
     if args.pretrained is not None:
         if args.latency_early_stopping and args.latency_propagate_zero:
             model = earlyzeroTTFS(channels.input, channels.output, args.beta, channels.size)
+        elif args.latency_propagate_zero:
+            model = zeroTTFS(channels.input, channels.output, args.beta, channels.size)
         elif args.latency_early_stopping:
             model = earlyTTFS(channels.input, channels.output, args.beta, channels.size)
         else:
             model = defaultTTFS(channels.input, channels.output, args.beta, channels.size)
         #model = CNV_SNN(channels.input, channels.output)
-        model.load_state_dict(torch.load(args.pretrained))
+        model.load_state_dict(torch.load(args.pretrained, map_location=device))
         model = model.to(device)
         if args.verbose:
             print("###Sanity Check###")
--- experiment/exec.sh
+++ experiment/exec.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-python execution.py --epoch 10 --single_gpu 1 --verbose --pretrained /home/mwkim/jupyter/root/F-TTFS/result/model/000_model_mnist_latency.weights
-python execution.py --epoch 10 --single_gpu 1 --verbose --latency_early_stopping --pretrained /home/mwkim/jupyter/root/F-TTFS/result/model/000_model_mnist_latency.weights
-python execution.py --epoch 10 --single_gpu 1 --verbose --latency_early_stopping --latency_propagate_zero --pretrained /home/mwkim/jupyter/root/F-TTFS/result/model/000_model_mnist_latency.weights
+#python evaluation.py --verbose --pretrained /home/mwkim/F-TTFS/result/model/000_model_mnist_latency.weights
+#python evaluation.py --verbose --latency_propagate_zero --pretrained /home/mwkim/F-TTFS/result/model/000_model_mnist_latency.weights
+#python evaluation.py --verbose --latency_early_stopping --pretrained /home/mwkim/F-TTFS/result/model/000_model_mnist_latency.weights
+#python evaluation.py --verbose --latency_early_stopping --latency_propagate_zero --pretrained /home/mwkim/F-TTFS/result/model/000_model_mnist_latency.weights
+
+python evaluation.py -d cifar10 --steps 256 --verbose --pretrained /home/mwkim/F-TTFS/result/model/129_model_cifar10_latency.weights
+python evaluation.py -d cifar10 --steps 256 --verbose --latency_propagate_zero --pretrained /home/mwkim/F-TTFS/result/model/129_model_cifar10_latency.weights
+python evaluation.py -d cifar10 --steps 256 --verbose --latency_early_stopping --pretrained /home/mwkim/F-TTFS/result/model/129_model_cifar10_latency.weights
+python evaluation.py -d cifar10 --steps 256 --verbose --latency_early_stopping --latency_propagate_zero --pretrained /home/mwkim/F-TTFS/result/model/129_model_cifar10_latency.weights
